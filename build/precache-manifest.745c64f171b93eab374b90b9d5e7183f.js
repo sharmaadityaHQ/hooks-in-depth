be2b1d582b1806fceffdfc2d61fd9ab0dbf1586d
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-04-08 06:29:35 +0530

    production ready

---
+++ build/precache-manifest.745c64f171b93eab374b90b9d5e7183f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "57bbc429d3de7a7d8250",
+    "url": "/static/js/main.57bbc429.chunk.js"
+  },
+  {
+    "revision": "26c28f085bc4378e5a8a",
+    "url": "/static/js/1.26c28f08.chunk.js"
+  },
+  {
+    "revision": "57bbc429d3de7a7d8250",
+    "url": "/static/css/main.a28b7420.chunk.css"
+  },
+  {
+    "revision": "e594cf38e4ab215c7a9bd92a6055718d",
+    "url": "/index.html"
+  }
+];
